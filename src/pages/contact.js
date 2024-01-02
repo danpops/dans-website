ec6a5484028dc5e75376ffa8ad24e957711d0e85
Dan Popovic <dan-popovic@hotmail.com>
2024-01-01 23:10:25 -0500

    fix (contact): default vars

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -9,8 +9,8 @@ import useContactForm from '@/hooks/useContactForm'
 export function getStaticProps () {
   const id = 'contact'
   const title = content.contact.title
-  const apiKey = process.env.API_KEY
-  const apiUrl = process.env.API_URL
+  const apiKey = process.env.API_KEY ?? ''
+  const apiUrl = process.env.API_URL ?? ''
   return { props: { id, title, apiKey, apiUrl } }
 }
 export default function ContactPage ({ apiKey, apiUrl }) {
