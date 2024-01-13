48f58c0ffa1b8a0fec5b06b8ef972e84a49dbc9f
Dan Popovic <dan-popovic@hotmail.com>
2024-01-13 11:57:51 -0500

    refactor (document): remove class -> function component

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,41 +1,35 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-export default class MyDocument extends Document {
-  static async getInitialProps (ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+export async function getInitialProps (ctx) {
+  const sheet = new ServerStyleSheet()
+  const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
-      }
-    } finally {
-      sheet.seal()
-    }
-  }
-
-  render () {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: App => props => sheet.collectStyles(<App {...props} />)
+    })
+  const initialProps = await Document.getInitialProps(ctx).finally(() => {
+    sheet.seal()
+  })
+  return {
+    ...initialProps,
+    styles: (
+      <>
+        {initialProps.styles}
+        {sheet.getStyleElement()}
+      </>
     )
   }
+}
+export default function MyDocument () {
+  return (
+    <Html>
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
