35929f12864b25389cae65bcc192c567b1649e1d
Dan Popovic <dan-popovic@hotmail.com>
2024-01-26 17:15:52 -0500

    feat (error): padding / centering

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,13 @@
 import { getWindowLayout } from '@/components/Layout'
 import { ContentContainer } from '@/components/Layout/styles'
 import { Heading4 } from '@/components/Text'
+import styled from 'styled-components'
 
+const ErrorContainer = styled(ContentContainer)`
+  justify-items: center;
+  padding: 2rem 1rem;
+  text-align: center;
+`
 export function getStaticProps () {
   const id = 'error'
   const title = 'Error'
@@ -9,12 +15,12 @@ export function getStaticProps () {
 }
 export default function NotFoundPage () {
   return (
-    <ContentContainer id='error-content'>
+    <ErrorContainer id='error-content'>
       <Heading4 style={{ color: 'red' }}>Page Not Found!</Heading4>
       <Heading4>
         Whatever you are looking for, you won't find it here...
       </Heading4>
-    </ContentContainer>
+    </ErrorContainer>
   )
 }
 
