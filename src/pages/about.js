26bf0b44d6afe96aa4c625cb1990670e4cb5b3cc
Dan Popovic <dan-popovic@hotmail.com>
2024-02-01 08:23:51 -0500

    feat (about): remove resume link

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,17 +4,15 @@ import { ContentContainer } from '@/components/Layout/styles'
 import { useTheme } from '@/components/Layout/ThemeProvider'
 import { BodyText } from '@/components/Text'
 import client from '@/cms/client'
-import { GET_ABOUT, GET_RESUME } from '@/cms/queries'
-import { ResumeLink } from '@/components/HeroBanner/styles'
+import { GET_ABOUT } from '@/cms/queries'
 
 export async function getServerSideProps () {
   const id = 'about'
   const data = await client.fetch(GET_ABOUT)
-  const resume = await client.fetch(GET_RESUME)
   const title = data.title
-  return { props: { id, title, data, resume } }
+  return { props: { id, title, data } }
 }
-export default function AboutPage ({ data, resume }) {
+export default function AboutPage ({ data }) {
   const { isDarkMode } = useTheme()
   const imgStyle = isDarkMode ? { filter: 'invert(100%)' } : {}
   return (
@@ -28,15 +26,6 @@ export default function AboutPage ({ data, resume }) {
       />
       <BodyText id='about-text'>{data.heading}</BodyText>
       <BodyText id='about-summary'>{data.summary}</BodyText>
-      <ResumeLink
-        id='resume-link'
-        target='_blank'
-        rel='noopener noreferrer'
-        href={resume.resumeURL}
-        style={{ justifySelf: 'center' }}
-      >
-        Resume
-      </ResumeLink>
     </ContentContainer>
   )
 }
