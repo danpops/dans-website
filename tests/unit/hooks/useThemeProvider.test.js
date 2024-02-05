9b32e311720e3098cbeeeee2a914a58b86c02cf8
Dan Popovic <dan-popovic@hotmail.com>
2024-02-05 12:35:36 -0500

    chore (unit): additional useThemeProvider test coverage

--- tests/unit/hooks/useThemeProvider.test.js
+++ tests/unit/hooks/useThemeProvider.test.js
@@ -1,19 +1,34 @@
-/* globals jest, describe, expect, it */
+/* globals describe, expect, it */
 
 const { act, renderHook } = require('@testing-library/react')
 const { default: useThemeProvider } = require('@/hooks/useThemeProvider')
 const { lightTheme, darkTheme } = require('@/lib/theme')
 
-jest.useFakeTimers()
-
 describe('useThemeProvider tests', () => {
-  it('should update theme using toggleTheme', async () => {
-    const { result, rerender } = renderHook(() => useThemeProvider())
+  it('should have light mode on initial state', () => {
+    const { result } = renderHook(() => useThemeProvider())
+
     expect(result.current.themeMode).toBe('light')
     expect(result.current.theme).toBe(lightTheme)
+    expect(result.current.isDarkMode).toBeFalsy()
+  })
+  it('should update theme using toggleTheme', async () => {
+    const { result, rerender } = renderHook(() => useThemeProvider())
+
+    // toggle theme to dark mode
     act(() => result.current.toggleTheme())
     await rerender()
+
     expect(result.current.themeMode).toBe('dark')
     expect(result.current.theme).toBe(darkTheme)
+    expect(result.current.isDarkMode).toBeTruthy()
+
+    // toggle theme to light mode
+    act(() => result.current.toggleTheme())
+    await rerender()
+
+    expect(result.current.themeMode).toBe('light')
+    expect(result.current.theme).toBe(lightTheme)
+    expect(result.current.isDarkMode).toBeFalsy()
   })
 })
