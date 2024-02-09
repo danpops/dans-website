10fab326a9c30e04a1a8b0d7fa7f18da68ad34fb
Dan Popovic <dan-popovic@hotmail.com>
2024-02-09 15:51:09 -0500

    feat (synth): synth note page

---
+++ src/pages/synth/note.js
@@ -0,0 +1,22 @@
+import { getWindowLayout } from '@/components/Layout'
+import Synth from '@/components/Synth'
+import useSynthNote from '@/hooks/useSynthNote'
+
+export function getStaticProps () {
+  const id = 'modular-synth'
+  const title = 'Modular Synth'
+  const props = {
+    id,
+    title,
+    showHomer: false,
+    showMarquee: false,
+    showCopyright: false
+  }
+  return { props }
+}
+export default function ModularSynthPage () {
+  const synth = useSynthNote()
+  return <Synth {...synth} />
+}
+
+ModularSynthPage.getLayout = getWindowLayout
