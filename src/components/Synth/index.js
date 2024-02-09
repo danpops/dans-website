787325a6a6806b7fe4bfaa571f2ecc4baab95b3c
Dan Popovic <dan-popovic@hotmail.com>
2024-02-09 15:50:58 -0500

    feat (synth): synth controls component

---
+++ src/components/Synth/index.js
@@ -0,0 +1,88 @@
+import { Analyzer } from '@/components/Synth/Analyzer'
+import {
+  PiWaveSawtooth,
+  PiWaveSquare,
+  PiWaveSine,
+  PiWaveTriangle,
+  PiPlayPauseFill
+} from 'react-icons/pi'
+import {
+  Button,
+  ControlContainer,
+  EffectContainer,
+  Slider,
+  SynthContainer
+} from './styles'
+
+const OSCILLATORS = [
+  { value: 'sine', label: 'Sine', icon: <PiWaveSine size={38} /> },
+  { value: 'square', label: 'Square', icon: <PiWaveSquare size={38} /> },
+  { value: 'sawtooth', label: 'Sawtooth', icon: <PiWaveSawtooth size={38} /> },
+  { value: 'triangle', label: 'Triangle', icon: <PiWaveTriangle size={38} /> }
+]
+export default function Synth (props) {
+  const {
+    Waveform,
+    frequency,
+    adjustFrequency,
+    isPlaying,
+    toggleSynth,
+    oscillatorType,
+    handleOscillatorChange,
+    freqAM,
+    freqFM,
+    changeAM,
+    changeFM
+  } = props
+
+  const renderOscillators = (item, index) => (
+    <Button
+      key={item.label}
+      $active={oscillatorType === item.value}
+      onClick={() => handleOscillatorChange(item.value)}
+    >
+      {item.icon}
+    </Button>
+  )
+
+  return (
+    <SynthContainer>
+      <Analyzer waveform={Waveform} />
+      <ControlContainer>
+        <Button $active={isPlaying} onClick={toggleSynth}>
+          <PiPlayPauseFill size={38} />
+        </Button>
+        {OSCILLATORS.map(renderOscillators)}
+      </ControlContainer>
+      <EffectContainer>
+        <Slider
+          type='range'
+          min='100'
+          max='500'
+          step='1'
+          defaultValue='300'
+          value={frequency}
+          onChange={e => adjustFrequency(parseFloat(e.target.value))}
+        />
+        <Slider
+          type='range'
+          min='0'
+          max='20'
+          step='0.1'
+          defaultValue='0'
+          value={freqAM}
+          onChange={e => changeAM(parseFloat(e.target.value))}
+        />
+        <Slider
+          type='range'
+          min='0'
+          max='50'
+          step='0.1'
+          defaultValue='0'
+          value={freqFM}
+          onChange={e => changeFM(parseFloat(e.target.value))}
+        />
+      </EffectContainer>
+    </SynthContainer>
+  )
+}
---
+++ src/components/Synth/styles.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components'
+import { borderStyle, hoverBorder } from '@/lib/global'
+import { fonts } from '@/lib/fonts'
+
+export const Button = styled.div`
+  display: grid;
+  align-items: center;
+  justify-items: center;
+  font-weight: 500;
+  font-family: 'MS Sans Serif';
+  font-size: ${fonts.sm};
+  text-align: center;
+  background-color: ${props => props.theme.secondary};
+  color: ${props => props.theme.font};
+  cursor: pointer;
+  padding: 0.5rem 1rem;
+  ${props => (props.$active ? hoverBorder(props) : borderStyle(props))}
+  outline: none;
+`
+export const ControlContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, auto);
+  grid-template-rows: 1fr;
+`
+export const EffectContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, auto);
+  grid-template-columns: 1fr;
+  row-gap: 1.75rem;
+`
+export const Slider = styled.input`
+  appearance: none;
+  -webkit-appearance: none;
+  width: 100%;
+  margin: 15px 0;
+  &:focus {
+    outline: none;
+  }
+  &::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 0.65rem;
+    cursor: pointer;
+    ${hoverBorder}
+    outline: none;
+  }
+  &::-webkit-slider-thumb {
+    border: none;
+    width: 1.15rem;
+    height: 2.45rem;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -1rem;
+    background: ${props => props.theme.button};
+    ${borderStyle}
+  }
+  &::-webkit-slider-runnable-track {
+    background: ${props => props.theme.button};
+    ${hoverBorder}
+    outline: none;
+  }
+  &::-moz-range-track {
+    border: none;
+    width: 100%;
+    height: 0.65rem;
+    cursor: pointer;
+  }
+  &::-moz-range-thumb {
+    border: none;
+    width: 1.15rem;
+    height: 2.45rem;
+    cursor: pointer;
+    appearance: none;
+    margin-top: -1rem;
+    background: ${props => props.theme.button};
+    ${borderStyle}
+  }
+`
+export const SynthContainer = styled.div`
+  display: grid;
+  grid-template-rows: repeat(3, minmax(0, auto));
+  row-gap: 2rem;
+  align-items: center;
+`
